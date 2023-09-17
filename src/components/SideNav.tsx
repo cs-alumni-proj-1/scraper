cc90dc2961679b9af50e7b1054c3798d83c59dab
Jimmy Lin <jimmy.lin@method.com>
2023-09-16 17:44:33 -0700

    feat: added arrows in sideNav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,13 +1,19 @@
 export default function SideNav() {
   return (
     <nav className="h-screen w-72 bg-navColor p-8">
-      <h1 className="text-4xl font-nats">Notifications</h1>
+      <div className="flex justify-between text-4xl font-nats">
+        <h1>Notifications </h1>
+        <h1>&#9662;</h1>
+      </div>
       <ul className="font-nats text-xl p-6">
         <li>View Banks</li>
         <li>Add Banks</li>
         <li>Remove Banks</li>
       </ul>
-      <h1 className="text-4xl font-nats">Email Address</h1>
+      <div className="flex justify-between text-4xl font-nats">
+        <h1>Email Address</h1>
+        <h1> &#x25BE;</h1>
+      </div>
       <ul className="font-nats text-xl p-6">
         <li>Edit Email</li>
         <li>Delete Email</li>
