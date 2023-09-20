83b9f32be2a99acf7f7d768dcdf5a81cec42f8a3
Josephine Chen <51842293+chenjosephine@users.noreply.github.com>
2023-09-20 19:01:57 -0400

    wip: add placeholder files

---
+++ server/models/users.models.ts
@@ -0,0 +1,17 @@
+import { z } from zod;
+
+export const BaseUserSchema = z
+  .object({
+    email: z
+      .string({
+        required_error: 'Email is required',
+        invalid_type_error: 'Email must be a string',
+      })
+      .email({ message: 'Invalid email address' }),
+    password: z.string({
+      required_error: 'Password is required',
+      invalid_type_error: 'Password must be a string',
+    })
+  })
+  .required()
+  .strict();
