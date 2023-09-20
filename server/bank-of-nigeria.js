0806b9ee19b7b91a97c272da36c13c15dbb6611d
Peter Kennedy <pkennedy97@gmail.com>
2023-09-20 13:15:06 -0400

    basic scraper complete

---
+++ server/bank-of-nigeria.js
@@ -0,0 +1,31 @@
+import { chromium } from 'playwright'
+
+const bankOfNigeria = 'https://www.cbn.gov.ng/documents/circulars.asp?beginrec=1&endrec=20&keyword=&from=&tod=';
+
+//test date before the more recent circular
+const previousDate = new Date(`2022-09-13`);
+async function scrapeBankOfNigeria() {
+    const browser = await chromium.launch({headless: false});
+    
+    const page = await browser.newPage();
+    await page.goto(bankOfNigeria);
+
+    const topCircular = page.locator('.dbasetable')
+    const topRowLocator = topCircular.locator('tr:nth-child(2)');
+    const publishedDate = await topRowLocator.locator('#publishedDt').textContent();
+
+    const partsOfDate = publishedDate.trim().split(' ')[1].split('/')
+
+    const year = parseInt(partsOfDate[2], 10);
+    const month = parseInt(partsOfDate[0], 10) - 1; //minus one b/c JS months are 0 indexed
+    const day = parseInt(partsOfDate[1], 10);
+
+    const date = new Date(year, month, day);
+
+    if(date.getTime() > previousDate.getTime()) {
+      console.log('new circular found!');
+    }
+    await browser.close();
+}
+
+await scrapeBankOfNigeria();
