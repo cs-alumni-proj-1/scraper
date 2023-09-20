ff876ff044faf472842dfd166cf067a46464113f
Josephine Chen <51842293+chenjosephine@users.noreply.github.com>
2023-09-19 22:29:07 -0400

    wip: add server config

---
+++ server/config/config.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DBNAME, DBPASS, DBUSER, ENV, HOST, PORT, URI } = process.env;
+
+export const databaseConfig = {
+  connectionString: URI,
+  host: HOST ?? "localhost",
+  database: DBNAME,
+  user: DBUSER,
+  password: DBPASS,
+  port: Number(PORT) || 5432,
+  max: 2,
+  idleTimeoutMillis: 120000,
+};
+
+const config = {
+  ...databaseConfig,
+  env: ENV,
+};
+
+export default config;
