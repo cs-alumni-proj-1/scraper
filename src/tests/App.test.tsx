6811eb27c1015bdf3e944f380f1900c5f2566fd7
Jimmy Lin <odylic.lin@gmail.com>
2023-09-22 17:03:35 -0700

    test: changed to getByText and removed the click which currently isn't being implemented

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import App from "../App";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
 
 it("should render Current Notifications", () => {
   render(<App />);
-  const message = screen.queryByText(/Current Notification/i);
+  const message = screen.getByText(/Current Notification/i);
   expect(message).toBeVisible();
 });
 
 it("renders the pagination bar", () => {
   render(<App />);
   const pageButton = screen.getByText("1");
-  userEvent.click(pageButton);
   expect(screen.getByText("1")).toBeVisible();
 });
