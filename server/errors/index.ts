1ec25292a35ca8cbe91395ae601e2a748f17ec4b
carmen hu <72165737+badwithnames@users.noreply.github.com>
2023-09-23 14:44:21 -0400

    add barrel

---
+++ server/errors/index.ts
@@ -0,0 +1,6 @@
+export * from "./base-error";
+export * from "./validation-errors";
+export * from "./not-found-errors";
+export * from "./server-errors";
+export * from "./authorization-errors";
+export * from "./database-errors";
