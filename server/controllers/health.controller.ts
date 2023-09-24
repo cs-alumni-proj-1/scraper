fa8f63205302681a9c77aeda0434ddd83f9c6f45
jiedong111 <jiedong111@gmail.com>
2023-09-24 03:36:08 -0400

    downloads circulars to local folder from link

--- server/controllers/health.controller.ts
+++ server/controllers/health.controller.ts
@@ -4,6 +4,8 @@ import Logger from "../utils/logger";
 import { StatusCodes } from "http-status-codes";
 import { assertIsError } from "../utils/guards";
 import { chromium, devices } from "playwright";
+import fs from 'fs';
+import axios from 'axios';
 
 export class HealthController {
   constructor() {
@@ -39,8 +41,21 @@ export class HealthController {
 
       const circulars = [];
 
-      for (const links of pdfLinks) {
-        const link = await links.getAttribute('href');
+      // const folderPath = `../circulars`;
+
+      for (let i = 0; i < pdfLinks.length; i++) {
+        const link = await pdfLinks[i].getAttribute('href');
+        const absoluteUrl = `https://centralbank.go.ke${link}`
+        //@ts-ignore
+        const file = await axios.get(absoluteUrl, { responseType: 'arraybuffer'});
+        const pdfPath = `server/circulars/circular${i}.pdf`;
+        console.log('WRITING FILE');
+        //@ts-ignore
+        await fs.writeFile(pdfPath, file.data, err => {
+          if(err) throw err;
+          console.log('SAVED!');
+        });
+
         circulars.push(link);
       }
 
@@ -54,3 +69,17 @@ export class HealthController {
     }
   }
 }
+
+// async function getStreamBuffer(stream: any) {
+//   console.log('GET BUFFER STREAM');
+//   const chunks = [];
+//   for await (const chunk of stream) {
+//     if (typeof chunk === 'number') {
+//       chunks.push(Buffer.alloc(chunk));
+//     } else {
+//       chunks.push(chunk);
+//     }
+//   }
+
+//   return Buffer.concat(chunks);
+// } 
